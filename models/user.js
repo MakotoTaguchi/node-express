e3c42838069d2ae8874e6830817726587f942bd2
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-03 13:05:01 +0900

    main

--- models/user.js
+++ models/user.js
@@ -1,26 +1,41 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const sequelize = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
+  const User = sequelize.define('User', {
     name: DataTypes.STRING,
     pass: DataTypes.STRING,
     mail: DataTypes.STRING,
     age: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }, {});
+  User.associate = function (models) {
+    // associations can be defined here
+  };
   return User;
 };
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   User.init({
+//     name: DataTypes.STRING,
+//     pass: DataTypes.STRING,
+//     mail: DataTypes.STRING,
+//     age: DataTypes.INTEGER
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
