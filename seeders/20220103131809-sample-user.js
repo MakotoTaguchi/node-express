0e27af62b69068acdd42cdaf466fc29604f7137e
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-03 22:30:05 +0900

    main

--- seeders/20220103131809-sample-user.js
+++ seeders/20220103131809-sample-user.js
@@ -2,6 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+        name: 'Taro',
+        pass: 'yamada',
+        mail: 'taro@yamada.jp',
+        age: 39,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Hanako',
+        pass: 'flower',
+        mail: 'hanako@flower.com',
+        age: 28,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Jiro',
+        pass: 'change',
+        mail: 'jiro@change.com',
+        age: 17,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Sachiko',
+        pass: 'hamada',
+        mail: 'sachiko@happy.jp',
+        age: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
     /**
      * Add seed commands here.
      *
@@ -10,10 +43,11 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
   down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
     /**
      * Add commands to revert seed here.
      *
