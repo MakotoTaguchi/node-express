9738e8971467b8de67ed6648ed23920346ae4e05
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-03 23:14:42 +0900

    main

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,25 @@ const db = require('../models/index');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  db.User.findAll().then(usrs => {
+  const id = req.query.id;
+  db.User.findAll({
+    where: {
+      id: id
+    }
+  }).then(usrs => {
     var data = {
       title: 'Users/Index',
       content: usrs
     }
     res.render('users/index', data);
   });
+  // db.User.findAll().then(usrs => {
+  //   var data = {
+  //     title: 'Users/Index',
+  //     content: usrs
+  //   }
+  //   res.render('users/index', data);
+  // });
 });
 
 module.exports = router;
