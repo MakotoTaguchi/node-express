6073d93342f010a200eac67cfa6755f5da306dd3
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-03 23:39:27 +0900

    main

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
+const {
+  Op
+} = require("sequelize");
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   const id = req.query.id;
   db.User.findAll({
     where: {
-      id: id
+      // id: id
+      id: {
+        [Op.lte]: id
+      }
     }
   }).then(usrs => {
     var data = {
