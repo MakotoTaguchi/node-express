a8a41b1d4c7e4e2583072846a1ccafe12f2e5c84
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-05 19:10:15 +0900

    main

--- models/user.js
+++ models/user.js
@@ -4,16 +4,50 @@ const sequelize = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    mail: DataTypes.STRING,
-    age: DataTypes.INTEGER
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    pass: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    mail: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+        min: 0
+      }
+    }
   }, {});
   User.associate = function (models) {
     // associations can be defined here
   };
   return User;
 };
+
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     name: DataTypes.STRING,
+//     pass: DataTypes.STRING,
+//     mail: DataTypes.STRING,
+//     age: DataTypes.INTEGER
+//   }, {});
+//   User.associate = function (models) {
+//     // associations can be defined here
+//   };
+//   return User;
+// };
 // const {
 //   Model
 // } = require('sequelize');
